725a6d8f3ccdf0abd74556c6cc786dbfc16d8af1
tooMuchLagWillKillYou <diego.bar@live.it>
2026-07-28 10:41:58 +0200

    menu page scaffold

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def write_classic():
     pass
 
 def main():
-    path = os.path.abspath("index.html")
-    converter.convert(f'file://{path}', './result.pdf')
+    path = os.path.abspath("./resources/index.html")
+    converter.convert(f'file://{path}', './output/result.pdf')
     print("Hello world")
 
 if __name__ == "__main__":
