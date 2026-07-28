82ebe9a96f4aafda18b3f89ea2e5a58d0c11f24f
tooMuchLagWillKillYou <diego.bar@live.it>
2026-07-28 08:27:49 +0200

    init main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+
+def write_front_page():
+    pass
+
+def write_specialty():
+    pass
+
+def write_classic():
+    pass
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
