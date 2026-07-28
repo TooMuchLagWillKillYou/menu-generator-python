69ce42792177c29a821b29a07f0e7e344ffb1195
tooMuchLagWillKillYou <diego.bar@live.it>
2026-07-28 09:12:16 +0200

    first basic test

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pyhtml2pdf import converter
+import os
 
 def write_front_page():
     pass
@@ -9,6 +11,8 @@ def write_classic():
     pass
 
 def main():
+    path = os.path.abspath("index.html")
+    converter.convert(f'file://{path}', './result.pdf')
     print("Hello world")
 
 if __name__ == "__main__":
